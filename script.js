e0260b3dbbf16c68a46e6087293e943a5760fdb9
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-05-15 13:25:21 -0500

    Creating variables
    We were creating variables with var, let, and const.

---
+++ script.js
@@ -0,0 +1,20 @@
+//alert("Hello world!");
+//promp(t("Enter your name:");
+//console.log("This is a message");
+//console.warn("This is a warning message");
+//console.error("This is an error message");
+
+// variables
+
+var x; // declaration of the var
+x=10; // assignation of the value
+//document.writeln(x);
+
+
+
+let job_Title='Software Developer';
+let geo_Location='Alabama';
+let partner_Name='None';
+let no_Children='1';
+
+document.writeln(`<p class=>You will be a ${job_Title}, in ${geo_Location}, and married to ${partner_Name} with ${no_Children} kid</p>`)
