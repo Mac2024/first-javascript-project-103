31290bc316386bd326d12d3d84de06feed6c72c1
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-05-15 13:47:20 -0500

    Doing operations and variables
    Using operations and variables

---
+++ exercise2.js
@@ -0,0 +1,13 @@
+let _name='Mario McGrady';
+let email='mcgradymario84@gmail.com';
+let password='Sprite24';
+let age='37';
+let country='US';
+let salary_month='$4000';
+let salary_yr='salary_month*12';
+
+document.writeln(`
+<p> Name: ${_name}</p>
+<p> Email: ${email}</p>
+<p> Salary: ${salary_yr}</p>
+`)
